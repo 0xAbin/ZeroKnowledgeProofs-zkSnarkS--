d07ee9841571c4f113f916d2f86c2fbe3a765902
Abin <68709126+AbinBVinod@users.noreply.github.com>
2022-12-26 13:57:01 +0530

    Update ZKPalgo.cpp

--- ZKPalgo.cpp
+++ ZKPalgo.cpp
@@ -2,51 +2,45 @@
 #include <cstdlib>
 #include <ctime>
 
-// This function represents the statement that we want to prove
+
 bool statement()
 {
-    // In this case, we want to prove that we know the value of a secret number
     int secret = 42;
 
-    // We return whether the secret number is even
+   
     return secret % 2 == 0;
 }
 
-// This function generates a random challenge value
+
 int generate_challenge()
 {
-    // Seed the random number generator
+   
     srand(time(nullptr));
 
-    // Generate a random number between 0 and 1
     return rand() % 2;
 }
 
-// This function represents the proof that we provide to the verifier
 int provide_proof(int challenge)
 {
-    // If the challenge is 0, we return the secret number
+    
     if (challenge == 0)
         return 42;
-    // If the challenge is 1, we return a random number
     else
         return rand();
 }
 
 int main()
 {
-    // The prover and verifier engage in the Zero Knowledge Proof protocol
-
-    // The verifier generates a challenge value
+  
     int challenge = generate_challenge();
 
-    // The prover provides a proof in response to the challenge
+   
     int proof = provide_proof(challenge);
 
-    // The verifier checks the proof to see if it is valid
+   
     bool is_proof_valid = (challenge == 0 && proof == 42) || (challenge == 1 && proof != 42);
 
-    // If the proof is valid, the verifier can be convinced that the statement is true
+  
     if (is_proof_valid)
         std::cout << "The prover has convinced the verifier that the statement is true." << std::endl;
     else
